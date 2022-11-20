74c4c0f31dc6814465469d0116c2271f4074b693
WesamRl <wesoorl@gmail.com>
2022-11-21 00:55:17 +0200

    HomeWork4

--- Branch2.py
+++ Branch2.py
@@ -18,47 +18,49 @@ def checkValueIsNumber(inputStr: str):
             return True
 
     return False
-num=input("Enter num")
-number1=checkValueIsNumber(num)
-number1=float(num)
-num2=input("Enter Second num")
-number2=checkValueIsNumber(num2)
-number2=float(num2)
-operat=input("Enter operat")
-if operat=="+" or operat=="1":
-    total=number1+number2
-    print("real total :", total)
-    print("nearly :", round(total))
-    print("integer value: ", int(total))
+num=input("Enter first number")
+if checkValueIsNumber(num)==True:
+   number1=float(num)
+   operat = input("Enter operat")
+   num2 = input("Enter Second number")
+   if checkValueIsNumber(num2)==True:
+       number2 = float(num2)
+       if operat == "+" or operat == "1":
+           total = number1 + number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
 
-elif operat == "2" or operat == "-":
-    total = number1 - number2
-    print("real total :", total)
-    print("nearly :", round(total))
-    print("integer value: ", int(total))
+       elif operat == "2" or operat == "-":
+           total = number1 - number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
 
-elif operat == "3" or operat == "*":
-     total = number1 * number2
-     print("real total :", total)
-     print("nearly :", round(total))
-     print("integer value: ", int(total))
+       elif operat == "3" or operat == "*":
+           total = number1 * number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
 
-elif operat == "4" or operat == "/":
-     total = number1 / number2
-     print("real total :", total)
-     print("nearly :", round(total))
-     print("integer value: ", int(total))
+       elif operat == "4" or operat == "/":
+           total = number1 / number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
 
-elif operat == "5" or operat == "^":
-     total = number1 ^ number2
-     print("real total :", total)
-     print("nearly :", round(total))
-     print("integer value: ", int(total))
+       elif operat == "5" or operat == "^":
+           total = number1 ^ number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
 
-elif operat == "6" or operat == "%":
-     total = number1 % number2
-     print("real total :", total)
-     print("nearly :", round(total))
-     print("integer value: ", int(total))
-else:
-    print("Error in your input operator")
+       elif operat == "6" or operat == "%":
+           total = number1 % number2
+           print("real total :", total)
+           print("nearly :", round(total))
+           print("integer value: ", int(total))
+       else:print("Error in your input operator")
+
+   else: print("invalid value")
+else:print("invalid value , Not number")
